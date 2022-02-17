f18f703db2bf95b7db61acae155ff515343aedc4
sadiyakhatoon339 <72649215+sadiyakhatoon339@users.noreply.github.com>
2022-02-18 00:58:03 +0530

    Add files via upload
    Implement a banking system using java.
    
    Create 3 sub class of Bank : SBI,BOI,ICICI
    
    All 4 should have following methods:
    
    getDetails which provide their specific details like rateofinterest etc
    printDetails of every bank.
    Every bank account should have a type (enum): SAVINGS, CURRENT
    Implement a feature to deduct amount from account. Throw InsufficientAmountException if amount being deducted is less than the current balance.
    Every banking transaction should be saved in a file of each bank's transaction log which would have all details of the transaction like (time of transaction,account number,amount withdrawn, amount before deduction, amount after deduction,transaction status (can also be enum), failure reason if any)

---
+++ Banking_Application/src/Application/Banking_Application.java
@@ -0,0 +1,244 @@
+package Application;
+
+import java.io.IOException;
+import java.io.*;
+import java.io.InputStreamReader;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.io.FileWriter;
+
+
+class Bank{
+
+    String Name;
+    String Branch_name;
+    double R_O_I;
+
+    enum type{
+        SAVING,CURRENT;
+    }
+}
+
+
+class InsufficientAmountException extends Exception{
+
+    InsufficientAmountException(){
+        super("Insufficient Balance to withdraw this amount");
+    }
+
+}
+
+
+
+class SBI extends Bank{
+    void getdetails(String name,String B_name,double roi){
+
+        Name=name;
+        Branch_name=B_name;
+        R_O_I=roi;
+
+
+    }
+
+
+    void printdetails(String name,String B_name,double roi)
+    {
+        System.out.println("Name of the bank is : "+name  +"  Branch name is: "+B_name +"  It provides the Rate of Interest of "+roi +"%" );
+    }
+
+    void account(int acc_no,double amt,double amt_withdrawn ){
+        int k=0;
+        try{
+
+            if(amt<amt_withdrawn){
+                throw new InsufficientAmountException();
+            }
+
+        }
+        catch(InsufficientAmountException e){
+            k=-1;
+            System.out.println(e.getMessage());
+
+        }
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+
+
+        try{
+            FileWriter my=new FileWriter("TransactionDetailsBOI.txt");
+            my.write("The account number is:"+acc_no +"\n");
+            my.write("The amount in the account :"+amt+"\n");
+            my.write("The amount withdrawn is :"+amt_withdrawn+"\n");
+            if(k==0){
+                my.write("The available amount in the account is :"+(amt-amt_withdrawn)+"\n");
+                my.write("Transaction successful"+"\n");
+                my.write("Transaction time  and date:"+now+"\n");
+
+
+            }
+            else
+                my.write("Transaction cancelled");
+               my.close();
+
+        }
+        catch(IOException e){
+            System.out.println("An error occured");
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+class BOI extends Bank{
+    void getdetails(String name,String B_name,double roi){
+
+        Name=name;
+        Branch_name=B_name;
+        R_O_I=roi;
+
+
+    }
+
+    void printdetails(String name,String B_name,double roi)
+    {
+        System.out.println("Name of the bank is : "+name  +"  Branch name is: "+B_name +"  It provides the Rate of Interest of "+roi +"%" );
+    }
+
+    void account(int acc_no,double amt,double amt_withdrawn ){
+        int k=0;
+        try{
+
+            if(amt<amt_withdrawn){
+                throw new InsufficientAmountException();
+            }
+
+        }
+        catch(InsufficientAmountException e){
+            k=-1;
+            System.out.println(e.getMessage());
+
+        }
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        try{
+            FileWriter my=new FileWriter("TransactionDetailsSBI.txt");
+            my.write("The account number is:"+acc_no +"\n");
+            my.write("The amount in the account :"+amt+"\n");
+            my.write("The amount withdran is :"+amt_withdrawn+"\n");
+            if(k==0){
+                my.write("The available amount in the account is :"+(amt-amt_withdrawn)+"\n");
+                my.write("Transaction successful"+"\n");
+                my.write("Transaction time  and date:"+now+"\n");
+
+
+            }
+            else
+                my.write("Transaction cancelled");
+                     my.close();
+
+
+        }
+        catch(IOException e){
+            System.out.println("An error occured");
+        }
+
+
+
+    }
+
+}
+
+class ICICI extends Bank{
+
+    void getdetails(String name,String B_name,double roi){
+
+        Name=name;
+        Branch_name=B_name;
+        R_O_I=roi;
+
+
+    }
+
+    void printdetails(String name,String B_name,double roi)
+    {
+        System.out.println("Name of the bank is : "+name  +".  Branch name is: "+B_name +".  It provides the Rate of Interest of "+roi +"%" );
+    }
+
+
+
+    void account(int acc_no,double amt,double amt_withdrawn ){
+        int k=0;
+        try{
+
+            if(amt<amt_withdrawn){
+                throw new InsufficientAmountException();
+            }
+
+        }
+        catch(InsufficientAmountException e){
+            k=-1;
+            System.out.println(e.getMessage());
+
+        }
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        try{
+            FileWriter my=new FileWriter("TransactionDetailsICICI.txt");
+            my.write("The account number is:"+acc_no +"\n");
+            my.write("The amount in the account :"+amt+"\n");
+            my.write("The amount withdran is :"+amt_withdrawn+"\n");
+            if(k==0){
+                my.write("The available amount in the account is :"+(amt-amt_withdrawn)+"\n");
+                my.write("Transaction successful"+"\n");
+                my.write("Transaction time  and date:"+now+"\n");
+
+
+            }
+            else
+                my.write("Transaction cancelled");
+
+            my.close();
+        }
+        catch(IOException e){
+            System.out.println("An error occured");
+        }
+    }
+}
+
+public class Banking_Application {
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        SBI ob=new SBI();
+        BOI ob1=new BOI();
+        ICICI ob2=new ICICI();
+
+        ob.getdetails("SBI","lucknowbranch",6);
+        ob.printdetails("SBI","lucknowbranch",6);
+        ob.account(1800101001,1800,80);
+
+        ob1.getdetails("BOI","bombayBranch",8);
+        ob1.printdetails("BOI","bombayBranch",8);
+        ob1.account(1400101001,1800,180000);
+
+        ob2.getdetails("ICICI","hyderabadbranch",7);
+        ob2.printdetails("ICICI","hyderabadbranch",7);
+        ob2.account(1300302001,500,70);
+
+
+
+
+    }
+}
+
